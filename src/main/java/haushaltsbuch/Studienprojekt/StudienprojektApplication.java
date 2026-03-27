c302dbf6083b8ef968e414272008bdff9ea344d6
yami1001 <yannismichel09@gmail.com>
2026-03-27 16:44:49 +0100

    Spring Boot Projekt hinzugefügt

---
+++ src/main/java/haushaltsbuch/Studienprojekt/StudienprojektApplication.java
@@ -0,0 +1,13 @@
+package haushaltsbuch.Studienprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudienprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudienprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/haushaltsbuch/Studienprojekt/StudienprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package haushaltsbuch.Studienprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudienprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
