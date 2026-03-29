96775fccf8649703dc31e09bdc13daede2fbef37
Paperhunter <25457206+Resch-Said@users.noreply.github.com>
2026-03-29 14:37:44 +0200

    Erlaubt das automatische generieren der Tabelle

--- src/main/java/haushaltsbuch/Studienprojekt/StudienprojektApplication.java
+++ src/main/java/haushaltsbuch/Studienprojekt/StudienprojektApplication.java
@@ -2,10 +2,17 @@ package haushaltsbuch.Studienprojekt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+// Hauptklasse der Spring Boot-Anwendung, die die Anwendung startet
 @SpringBootApplication
+@EntityScan(basePackages = "model")
+@EnableJpaRepositories(basePackages = "model")
 public class StudienprojektApplication {
 
+	// Main-Methode, die die Spring Boot-Anwendung startet
 	public static void main(String[] args) {
 		SpringApplication.run(StudienprojektApplication.class, args);
 	}
