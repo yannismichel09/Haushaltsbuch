dab0c42f7d0da1497fee0cda1ae9066bfaa8ab6c
Paperhunter <25457206+Resch-Said@users.noreply.github.com>
2026-04-05 19:36:05 +0200

    Update DBAccessTestUser.java

--- src/test/java/haushaltsbuch/DBAccessTestUser.java
+++ src/test/java/haushaltsbuch/DBAccessTestUser.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
