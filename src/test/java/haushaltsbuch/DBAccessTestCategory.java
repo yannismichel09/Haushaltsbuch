059808955add66fa206a6618c985d914218367ef
Paperhunter <25457206+Resch-Said@users.noreply.github.com>
2026-04-05 20:25:25 +0200

    Erstelle die testCreateCategory

--- src/test/java/haushaltsbuch/DBAccessTestCategory.java
+++ src/test/java/haushaltsbuch/DBAccessTestCategory.java
@@ -3,6 +3,7 @@ package haushaltsbuch;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,28 @@ public class DBAccessTestCategory {
 
     @Autowired
     private DBAccessCategory dbAccess;
+
+    // Testet das Erstellen einer neuen Kategorie und validiert die gespeicherten Werte
+    @Test
+    void testCreateCategory() {
+        String name = "testCreateCategory";
+        String description = "testCreateCategoryDescription";
+        String color = "testCreateCategoryColor";
+        Double limit = 200.0;
+
+        Category category = dbAccess.createCategory(name, description, color, limit);
+
+        assertNotNull(category);
+        assertNotNull(category.getCategoryId());
+        assertEquals(name, category.getCategoryName());
+        assertEquals(description, category.getCategoryDescription());
+        assertEquals(color, category.getCategoryColor());
+        assertEquals(limit, category.getCategoryLimit());
+
+        Category persistedCategory = dbAccess.getCategoryById(category.getCategoryId());
+        assertNotNull(persistedCategory);
+        assertEquals(category, persistedCategory);
+    }
 
     // Testet das Abrufen einer vorhandenen Kategorie anhand ihrer Id
     @Test
