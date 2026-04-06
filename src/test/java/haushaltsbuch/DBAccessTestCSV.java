0b9dc96bb8381f808a378c951393b10ca0f01e04
yami1001 <yannismichel09@gmail.com>
2026-04-06 08:41:29 +0200

    85-exportfilteredcategoriestocsv-test

--- src/test/java/haushaltsbuch/DBAccessTestCSV.java
+++ src/test/java/haushaltsbuch/DBAccessTestCSV.java
@@ -3,15 +3,16 @@ package haushaltsbuch;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import dbaccess.DBAccessCSV;
+import dbaccess.DBAccessCategory;
 import dbaccess.DBAccessTransaction;
 import haushaltsbuch.Studienprojekt.StudienprojektApplication;
+import model.Category;
 
 @Transactional
 @SpringBootTest(classes = StudienprojektApplication.class)
@@ -22,6 +23,9 @@ class DBAccessTestCSV {
 
     @Autowired
     private DBAccessTransaction dbAccessTransaction;
+
+    @Autowired
+    private DBAccessCategory dbAccessCategory;
 
     // Testet das Exportieren gefilterter Transaktionen in CSV
     @Test
@@ -37,5 +41,24 @@ class DBAccessTestCSV {
         
         assertFalse(csv.contains("101"));
         assertFalse(csv.contains("blub"));
+    }
+
+    // Testet das Exportieren gefilterter Kategorien in CSV
+    @Test
+    void testExportFilteredCategoriesToCsv() {
+
+        Category category = dbAccessCategory.createCategory("testExportCategory", "testExportDescription", "red", 100.0);
+
+        String csv = dbAccessCSV.exportFilteredCategoriesToCsv(category.getCategoryId(), null, null, 0.0, Double.MAX_VALUE);
+
+        assertNotNull(csv);
+        assertTrue(csv.contains("ID,Name,Description,Color,Limit"));
+        assertTrue(csv.contains("testExportCategory"));
+        assertTrue(csv.contains("testExportDescription"));
+        assertTrue(csv.contains("red"));
+        assertTrue(csv.contains("100.0"));
+  
+        assertFalse(csv.contains("blub"));
+        assertFalse(csv.contains("999.0"));
     }
 }
