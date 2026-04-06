9ec160ae244974180b193e95c4864f5cd04d5237
yami1001 <yannismichel09@gmail.com>
2026-04-06 14:51:02 +0200

    logs hinzugefügt

--- src/main/java/scheduled/ScheduledTasks.java
+++ src/main/java/scheduled/ScheduledTasks.java
@@ -3,6 +3,8 @@ package scheduled;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,16 +18,20 @@ public class ScheduledTasks {
     @Autowired
     private DBBackgroundTasks dbBackgroundTasks;
 
+    private static final Logger log=LoggerFactory.getLogger(ScheduledTasks.class);
+
     private List<Category> budgetWarnings = new ArrayList<>();
     private boolean isNegativeBalance = false;
 
     @Scheduled(fixedRate = 60_000)
     public void checkBudgetLimit() {
+        log.info("checkBudgetLimit");
         budgetWarnings = dbBackgroundTasks.backgroundCheckBudgetLimit();
     }
 
     @Scheduled(fixedRate = 60_000)
     public void checkNetBalance() {
+        log.info("checkNetBalance");
         isNegativeBalance = dbBackgroundTasks.backgroundCheckNetBalance();
     }
 
