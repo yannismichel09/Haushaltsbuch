b524668822ad9adc52efc10c1b6d3e30bcac7f44
yami1001 <yannismichel09@gmail.com>
2026-04-06 14:47:12 +0200

    112-scheduledtasks

--- src/main/java/haushaltsbuch/Studienprojekt/StudienprojektApplication.java
+++ src/main/java/haushaltsbuch/Studienprojekt/StudienprojektApplication.java
@@ -5,13 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 // Hauptklasse der Spring Boot-Anwendung, die die Anwendung startet
 @SpringBootApplication
 @EntityScan(basePackages = "model")
 @EnableJpaRepositories(basePackages = "model")
-@ComponentScan(basePackages = {"haushaltsbuch.Studienprojekt", "dbaccess"})
+@ComponentScan(basePackages = {"haushaltsbuch.Studienprojekt", "dbaccess", "dbbackground", "scheduled"})
+@EnableScheduling
 public class StudienprojektApplication {
 
 	// Main-Methode, die die Spring Boot-Anwendung startet
--- src/main/java/scheduled/ScheduledTasks.java
+++ src/main/java/scheduled/ScheduledTasks.java
@@ -1,5 +1,34 @@
 package scheduled;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import dbbackground.DBBackgroundTasks;
+import model.Category;
+
+@Component
 public class ScheduledTasks {
 
+    @Autowired
+    private DBBackgroundTasks dbBackgroundTasks;
+
+    private List<Category> budgetWarnings = new ArrayList<>();
+    private boolean isNegativeBalance = false;
+
+    @Scheduled(fixedRate = 60_000)
+    public void checkBudgetLimit() {
+        budgetWarnings = dbBackgroundTasks.backgroundCheckBudgetLimit();
+    }
+
+    @Scheduled(fixedRate = 60_000)
+    public void checkNetBalance() {
+        isNegativeBalance = dbBackgroundTasks.backgroundCheckNetBalance();
+    }
+
+    public List<Category> getBudgetWarnings() { return budgetWarnings; }
+    public boolean getIsNegativeBalance() { return isNegativeBalance; }
 }
