825358378db8701d1b768667fd416499f3067d64
Paperhunter <25457206+Resch-Said@users.noreply.github.com>
2026-04-10 18:17:08 +0200

    Setze von int auf Integer

--- src/main/java/dbaccess/DBAccessCSV.java
+++ src/main/java/dbaccess/DBAccessCSV.java
@@ -27,7 +27,7 @@ public class DBAccessCSV {
 	}
 
 	// Methode zum Exportieren aller Kategorien in eine CSV-Datei
-	public String exportFilteredCategoriesToCsv(int categoryId, String keyword,String categoryColor, Double amountMin, Double amountMax) {
+    public String exportFilteredCategoriesToCsv(Integer categoryId, String keyword,String categoryColor, Double amountMin, Double amountMax) {
 
         DBAccessCategory dbAccessCategory = new DBAccessCategory(entityManager);
         List<Category> categories = dbAccessCategory.getFilteredCategories(categoryId, keyword, categoryColor, amountMin, amountMax);
