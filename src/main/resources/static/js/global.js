195adf56db1b3f9712f3ca050a274e495dfee437
yami1001 <yannismichel09@gmail.com>
2026-04-20 23:12:52 +0200

    257-localstorage-verwenden

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,6 +1,7 @@
-let globalToken = null;
-let currentUser = null;
-let currentUserId = null;
+let globalToken = localStorage.getItem("globalToken") || null;
+let currentUser = JSON.parse(localStorage.getItem("currentUser")) || null;
+let currentUserId = window.currentUser ? window.currentUser.userId : null;
+
 const CATEGORY_BASE_PATH  = "/categories";
 const CSV_BASE_PATH = "/csv";
 const DASHBOARD_BASE_PATH = "/dashboard";
@@ -8,3 +9,17 @@ const SETTINGS_BASE_PATH = "/settings";
 const TRANSACTION_BASE_PATH = "/transactions";
 const USER_BASE_PATH = "/users";
 const WARNING_BASE_PATH = "/warnings";
+
+function saveSession(token, user) {
+    localStorage.setItem("globalToken", token);
+    localStorage.setItem("currentUser", JSON.stringify(user));
+    
+    globalToken = token;
+    currentUser = user;
+    currentUserId = user.userId;
+}
+
+function logout() {
+    localStorage.clear();
+    window.location.href = "welcome.html";
+}
